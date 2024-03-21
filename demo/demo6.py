8eb6c2e1871cc1425e1b22b64aa366017e44326d
Keshava Narayan <74804559+keshavanarayan@users.noreply.github.com>
2024-03-21 17:14:53 +0530

    setup conda

--- demo/demo6.py
+++ demo/demo6.py
@@ -1,9 +1,5 @@
 import ifcopenshell
-import ifcopenshell.util.element
 import ifcopenshell.util.representation
-import ifcopenshell.api
-import ifcopenshell.api.context.add_context
-import ifcopenshell.entity_instance
 
 # Open the IFC file
 file = ifcopenshell.open("ifc/rac_advanced_sample_project.ifc")
@@ -13,27 +9,27 @@ door = file.by_type("IfcDoor")[0]
 
 contexts = file.by_type("IfcRepresentationContext")
 
-context = contexts[0]
-
 for each in contexts:
     if each.ContextIdentifier == "Body":
         context = each
 
-#print(context)
-
 
 nested_elements = ifcopenshell.util.representation.get_representation(door,context)
-#print (nested_elements)
 
-# Print the names of the nested elements
+
 for element in nested_elements:
     #print(element)
-    print(ifcopenshell.entity_instance.is_a(element))
-    """
-    if element.is_a("IfcShapeRepresentation"):
-        print(element.RepresentationType)
-        #print(element.Name)
-"""
+    if type(element) == ifcopenshell.entity_instance:
+        #print(element.is_a())
+        print(element.RepresentationsInContext[0].Items[0].FbsmFaces[0].CfsFaces[0].Bounds[0].Bound.Polygon)
+        print (".........................")
+        continue
+    else:
+        #print(dir(element))
+        print (".........................")
+        print (".........................")
+        continue
+
 
 
 
